4b78cc2d039f5c933bc81b294b3e58fa61362581
KRISHNAMSETTI JAYA KRISHNA VAMSI <83599507+jayakrishnavamsi24@users.noreply.github.com>
2023-05-26 15:39:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import NotFound from './components/NotFound'
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
     <Switch>
       <Route exact path="/login" component={LoginForm} />
       <Route exact path="/" component={Home} />
@@ -17,7 +16,6 @@ const App = () => (
       <Route exact path="/cart" component={Cart} />
       <Route component={NotFound} />
     </Switch>
-  </BrowserRouter>
 )
 
 export default App
